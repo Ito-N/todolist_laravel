f5639a82504e25f7108b40dc8acd74345b1e1f8b
Ito-N <theme4087abcdef@yahoo.co.jp>
2021-02-14 16:20:26 +0900

    feat: 投稿一覧画面、投稿詳細画面作成

---
+++ src/resources/views/front/posts/show.blade.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var \App\Models\Post $post
+ */
+$title = '投稿詳細';
+?>
+@extends('front.layouts.base')
+
+@section('content')
+    <div class="card-header">{{ $title }}</div>
+    <div class="card-body">
+        <h2>{{ $post->title }}</h2>
+        <time>{{ $post->published_at->format('Y年m月d日') }}</time>
+        <div>{!! nl2br(e($post->body)) !!}</div>
+        <div class="btn btn-secondary"><a href="{{ route('front.posts.index') }}">'一覧へ戻る'</a></div>
+    </div>
+@endsection
